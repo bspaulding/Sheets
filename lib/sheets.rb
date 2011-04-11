2f23259761b37a77c712954c0f0bf712932ad164
Bradley Spaulding <brad.spaulding@gmail.com>
2011-04-11 01:09:50 -0400

    Fixed absolute path requires.

--- lib/sheets.rb
+++ lib/sheets.rb
@@ -5,15 +5,17 @@ module Sheets
   class UnsupportedSpreadsheetFormatError < StandardError; end
 end
 
+lib_path = File.expand_path(File.dirname(__FILE__))
+
 # Load Parsers
-require File.join 'lib', 'sheets', 'parseable.rb'
-require File.join 'lib', 'sheets', 'parsers', 'base.rb'
-Dir[ File.join 'lib', 'sheets', 'parsers', '*_parser.rb' ].each {|file| require file }
+require File.join lib_path, 'sheets', 'parseable.rb'
+require File.join lib_path, 'sheets', 'parsers', 'base.rb'
+Dir[ File.join lib_path, 'sheets', 'parsers', '*_parser.rb' ].each {|file| require file }
 
 # Load Renderers
-require File.join 'lib', 'sheets', 'renderable.rb'
-require File.join 'lib', 'sheets', 'renderers', 'base.rb'
-Dir[ File.join 'lib', 'sheets', 'renderers', '*_renderer.rb' ].each {|file| require file }
+require File.join lib_path, 'sheets', 'renderable.rb'
+require File.join lib_path, 'sheets', 'renderers', 'base.rb'
+Dir[ File.join lib_path, 'sheets', 'renderers', '*_renderer.rb' ].each {|file| require file }
 
 # Load Sheets::Base
-require File.join 'lib', 'sheets', 'base.rb'
+require File.join lib_path, 'sheets', 'base.rb'
