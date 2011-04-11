c5d739f764879da6a72e26ac4bf9f0c141238766
Bradley Spaulding <brad.spaulding@gmail.com>
2011-04-11 01:10:27 -0400

    Sheets::Base handles StringIO objects. You must explicitly pass the format.

--- lib/sheets/base.rb
+++ lib/sheets/base.rb
@@ -5,11 +5,11 @@ module Sheets
 
     def initialize(file, options = {})
       file = File.open(file.to_s) unless file.respond_to? :read
-      options[:format] ||= File.basename(file.path).split('.')[-1]
+      options[:format] ||= File.basename(file.path || "").split('.')[-1]
       
       @data = file.read
       @extension = options[:format].to_s
-      @file_path = File.expand_path(file.path)
+      @file_path = File.expand_path(file.path || "")
 
       raise UnsupportedSpreadsheetFormatError, "Couldn't find a parser for the '#{@extension}' format." if parser.nil?
     end
