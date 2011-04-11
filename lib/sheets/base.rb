08c9630a8f309680c83c628b6fca3233c83669a5
Bradley Spaulding <brad.spaulding@gmail.com>
2011-04-11 01:30:41 -0400

    Added initializer support for array input, skipping parsing.

--- lib/sheets/base.rb
+++ lib/sheets/base.rb
@@ -4,6 +4,12 @@ module Sheets
     include Renderable
 
     def initialize(file, options = {})
+      if file.is_a?(Array)
+        @data = file
+        @extension = options[:format].to_s
+        return
+      end
+
       file = File.open(file.to_s) unless file.respond_to? :read
       options[:format] ||= File.basename(file.path || "").split('.')[-1]
       
@@ -12,6 +18,11 @@ module Sheets
       @file_path = File.expand_path(file.path || "")
 
       raise UnsupportedSpreadsheetFormatError, "Couldn't find a parser for the '#{@extension}' format." if parser.nil?
+    end
+
+    alias_method :default_to_array, :to_array
+    def to_array
+      @data.is_a?(Array) ? @data : default_to_array
     end
   end
 end
