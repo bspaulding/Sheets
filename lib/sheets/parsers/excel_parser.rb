aad449cbcabbcfbdf8da140875e33feb050f7e34
Bradley Spaulding <brad.spaulding@gmail.com>
2011-04-26 15:47:14 -0400

    Implemented ExcelParser#to_array.

--- lib/sheets/parsers/excel_parser.rb
+++ lib/sheets/parsers/excel_parser.rb
@@ -2,6 +2,8 @@ class Sheets::Parsers::ExcelParser < Sheets::Parsers::Base
   parses :xls
 
   def to_array
-    
+    workbook = Spreadsheet.open(@file_path)
+    worksheet = workbook.worksheet(0)
+    worksheet.collect {|row| row.collect {|cell| cell.to_s } }
   end
 end
