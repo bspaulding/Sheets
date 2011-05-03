4a7732389d71f2c8ced50407cc9744bfbb86d556
Bradley Spaulding <brad.spaulding@gmail.com>
2011-05-03 10:36:10 -0400

    Removed UID string from generated class names, was causing NameErrors on 1.8.7.

--- test/generators/test_classes.rb
+++ test/generators/test_classes.rb
@@ -1,7 +1,7 @@
 def test_classes_for_collection(collection, &block)
   collection.each do |object|
     test_class = Class.new(Test::Unit::TestCase)
-    Object.const_set("Test#{object.to_s.split(':')[-1]}Interface_#{Time.now.strftime('%Y%m%d%H%M%S%N')}", test_class)
+    Object.const_set("Test#{object.to_s.split(':')[-1]}Interface", test_class)
     test_class.instance_exec(object, &block)
   end
 end
