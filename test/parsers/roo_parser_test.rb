073ee72204361408c3e84a1af46744f71c14310d
Bradley Spaulding <brad.spaulding@gmail.com>
2011-11-29 13:57:47 -0500

    Updated NokogiriParser to handle booleans, dates and numbers.

--- test/parsers/roo_parser_test.rb
+++
@@ -1,8 +0,0 @@
-class TestRooParser < Test::Unit::TestCase
-  
-  def test_roo_date_format_issue
-    sheet = Sheets::Base.new( 'test/data/roo_issue_28885_test.xlsx' )
-
-    assert_equal "2011-01-10", sheet.to_array[1][0]
-  end
-end
